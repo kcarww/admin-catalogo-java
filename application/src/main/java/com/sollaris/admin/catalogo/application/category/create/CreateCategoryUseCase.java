f617dedaa67c76aaab2def7d491b62d554ea87d3
kcarww <carlos.oe.cc@gmail.com>
2025-05-28 16:36:06 -0300

    Added notification class and instaled vavr

--- application/src/main/java/com/sollaris/admin/catalogo/application/category/create/CreateCategoryUseCase.java
+++ application/src/main/java/com/sollaris/admin/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -1,7 +1,9 @@
 package com.sollaris.admin.catalogo.application.category.create;
 
 import com.sollaris.admin.catalogo.application.UseCase;
+import com.sollaris.admin.catalogo.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 
-public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>> {
 
 }
